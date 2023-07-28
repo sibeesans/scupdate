ede0a86cb92863a3b4a6f317c5716d818264777e
YOGZKNTL <138388899+YOGZKNTL@users.noreply.github.com>
2023-07-29 02:16:10 +0700

    Update yogzasu.sh

--- yogzasu.sh
+++ yogzasu.sh
@@ -216,7 +216,6 @@ echo ""
 wget -q https://raw.githubusercontent.com/YOGZKNTL/scupdate/main/tools.sh;chmod +x tools.sh;./tools.sh
 rm tools.sh
 clear
-figlet 'AUTOSCRIPT'  | lolcat
     echo -e "${red}    ♦️${NC} ${green} CUSTOM SETUP DOMAIN VPS     ${NC}"
     echo -e "${red}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
     echo "1. Use Domain From Script / Gunakan Domain Dari Script"
