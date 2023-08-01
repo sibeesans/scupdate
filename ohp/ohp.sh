fdc2dca4034993fd5d0ed10e029691c8f9424e29
YOGZKNTL <138388899+YOGZKNTL@users.noreply.github.com>
2023-08-02 04:20:42 +0700

    Update ohp.sh

--- ohp/ohp.sh
+++ ohp/ohp.sh
@@ -21,7 +21,7 @@ Port_OHP='8585';
 
 #Installing ohp Server
 cd 
-wget -O /usr/local/bin/ohpd "https://raw.githubusercontent.com/scupdate/main/ohp/ohpd"
+wget -O /usr/local/bin/ohpd "https://raw.githubusercontent.com/YOGZKNTL/scupdate/main/ohp/ohpd"
 chmod +x /usr/local/bin/ohpd
 
 #Buat Service Untuk OHP Dropbear
