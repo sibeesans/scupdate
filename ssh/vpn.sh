8ebd60df687bfd2e368119e1c4c68048585a1237
sibeesans <105978585+sibeesans@users.noreply.github.com>
2023-08-05 20:18:43 +0700

    Update vpn.sh

--- ssh/vpn.sh
+++ ssh/vpn.sh
@@ -25,7 +25,7 @@ fi
 # Mod By SL
 # ==================================================
 # Link Hosting Kalian
-woks="raw.githubusercontent.com/YOGZKNTL/scupdate/main/ssh"
+woks="raw.githubusercontent.com/sibeesans/scupdate/main/ssh"
 
 # initialisasi var
 export DEBIAN_FRONTEND=noninteractive
