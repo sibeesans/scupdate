9f376b61604110ee4f51e47bf59f80cd8dc3b27e
sibeesans <105978585+sibeesans@users.noreply.github.com>
2023-08-05 20:29:46 +0700

    Update shadowsocks.sh

--- xray/shadowsocks.sh
+++ xray/shadowsocks.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 clear
-red() { echo -e "\\033[32;1m${*}\\033[0m"; }
-ipsaya=$(wget -qO- ipinfo.io/ip)
-data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/YOGZKNTL/regip/main/ip"
-checking_sc() {
-  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
-  if [[ $date_list < $useexp ]]; then
-    echo -ne
-  else
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e ""
-    echo -e "            ${RED}PERMISSION DENIED !${NC}"
-    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
-    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
-    echo -e "             \033[0;33mContact Admin :${NC}"
-    echo -e "      \033[0;36mTelegram${NC} t.me/WokszXD"
-    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6285279323958"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    exit
-  fi
-}
-checking_sc
 clear
 grenbo="\e[92;1m"
 NC='\033[0m'
