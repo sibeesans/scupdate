4168b7d0b61c2be3b147c560c93dfa478e157379
sibeesans <105978585+sibeesans@users.noreply.github.com>
2023-08-05 20:02:31 +0700

    Update add-bot-panel.sh

--- menu/add-bot-panel.sh
+++ menu/add-bot-panel.sh
@@ -28,33 +28,6 @@ else
    status_bot="${RED}Offline${NC} "
 fi
 #####
-KIRI="\033[1;32m>\033[1;33m>\033[1;31m>\033[1;31m$NC"
-ipsaya=$(wget -qO- ipinfo.io/ip)
-data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/YOGZKNTL/regip/main/ip"
-checking_sc() {
-  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
-  if [[ $date_list < $useexp ]]; then
-    echo -ne
-  else
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e ""
-    echo -e "            ${RED}PERMISSION DENIED !${NC}"
-    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
-    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
-    echo -e "             \033[0;33mContact Admin :${NC}"
-    echo -e "      \033[0;36mTelegram${NC} t.me/Kytxz"
-    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6281774970898"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    exit
-  fi
-}
-checking_sc
-
-echo -e "\e[32mloading...\e[0m"
 clear
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear 
@@ -76,7 +49,7 @@ echo -e ""
 read -p " Select menu :  "  opt
 echo -e   ""
 case $opt in
-01 | 1) clear ; wget https://raw.githubusercontent.com/YOGZKNTL/scupdate/main/kyt.sh && chmod +x kyt.sh && ./kyt.sh ;;
+01 | 1) clear ; wget https://raw.githubusercontent.com/sibeesans/scupdate/main/kyt.sh && chmod +x kyt.sh && ./kyt.sh ;;
 02 | 2) clear ; hapus-bot ;;
 02 | 3) clear ; stop-bot ;;
 00 | 4) clear ; restart-bot ;;
