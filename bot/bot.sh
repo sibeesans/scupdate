fa6a2bca8ad02d9f82b236ec99b0a75264172a46
sibeesans <105978585+sibeesans@users.noreply.github.com>
2023-08-05 20:35:10 +0700

    Update bot.sh

--- bot/bot.sh
+++ bot/bot.sh
@@ -9,33 +9,13 @@ GREEN='\033[0;32m'
 grenbo="\033[1;95m"
 YELL='\033[1;33m'
 BGX="\033[42m"
-UPDATE="https://raw.githubusercontent.com/YOGZKNTL/main/up.sh"
-BOT="https://raw.githubusercontent.com/YOGZKNTL/bot/main/bot.sh"
+UPDATE="https://raw.githubusercontent.com/sibeesans/main/up.sh"
+BOT="https://raw.githubusercontent.com/sibeesans/bot/main/bot.sh"
 ipsaya=$(wget -qO- ipinfo.io/ip)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/YOGZKNTL/regip/main/ip"
-checking_sc() {
-  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
-  if [[ $date_list < $useexp ]]; then
-    echo -ne
-  else
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e ""
-    echo -e "            ${RED}PERMISSION DENIED !${NC}"
-    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
-    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
-    echo -e "             \033[0;33mContact Admin :${NC}"
-    echo -e "      \033[0;36mTelegram${NC} t.me/YSSHstore"
-    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6281215360549"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    exit
-  fi
-}
-checking_sc
-
+clear
+echo -e ""
 echo -e "\033[96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo -e " \e[1;971m                      MENU BOT                \e[0m"
 echo -e "\033[96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
